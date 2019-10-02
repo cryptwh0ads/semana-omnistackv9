a0bc5aebd4cf4f194ffb21a838f2034993fbff1e
Vinicius MRS <vinicius@mrseditorial.cloud>
2019-10-02 10:04:57 -0300

    Make styled login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import logo from "./assets/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      <img src={logo} alt="logo" />
 
-      </header>
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre{" "}
+          <strong>talentos</strong> para sua empresa.
+        </p>
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input type="email" id="email" placeholder="Seu melhor e-mail" />
+          <button type="submit" className="btn">
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
