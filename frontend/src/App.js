5e2057ea5d34a39c07c73fa0d1f1f53bc16b6062
Vinicius MRS <vinicius@mrseditorial.cloud>
2019-10-02 10:52:16 -0300

    Create folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import api from './services/api';
 import "./App.css";
 
 import logo from "./assets/logo.svg";
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    // console.log('Hello World!');
+    // console.log(email);
+
+    const response = await api.post('/sessions', { email });
+
+    // console.log(response)
+
+    const { _id } = response.data;
+
+    // console.log(_id);
+    localStorage.setItem('user', _id);
+  }
+
+
   return (
     <div className="container">
       <img src={logo} alt="logo" />
@@ -13,9 +32,15 @@ function App() {
           Ofereça <strong>spots</strong> para programadores e encontre{" "}
           <strong>talentos</strong> para sua empresa.
         </p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
-          <input type="email" id="email" placeholder="Seu melhor e-mail" />
+          <input
+            type="email"
+            id="email"
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
           <button type="submit" className="btn">
             Entrar
           </button>
